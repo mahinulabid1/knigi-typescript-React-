d7f06778efae891e5332c0421d6d42c009c88289
Mahinul <himahinulabid@gmail.com>
2024-02-08 22:57:50 +0600

    feat: add module alias feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      ui: `${path.resolve(__dirname, "./src/ui/")}`,
+      template: `${path.resolve(__dirname, "./src/template/")}`,
+    }
+  }
 })
