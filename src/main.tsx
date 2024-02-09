1fc855f79bf62076f43b1726c3409235941c931f
Mahinul <himahinulabid@gmail.com>
2024-02-09 08:46:36 +0600

    modify: remove unused/extra code

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './assets/css/globalStyles.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import routeHandler from './routeHandler';
 import './assets/css/globalStyles.css';
 
