51a9bd6a16226eb3ea5f931b7a324783338cdbb8
Mahinul <himahinulabid@gmail.com>
2024-02-10 10:59:34 +0600

    feat: configure redux at main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import routeHandler from './routeHandler';
+import { store } from '../store/store';
+import { Provider } from 'react-redux'
 import './assets/css/globalStyles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={routeHandler} />
+    <Provider store={store}>
+      <RouterProvider router={routeHandler} />
+    </Provider>
   </React.StrictMode>
 )
