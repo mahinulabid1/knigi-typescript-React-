e04f51b85d8bf481b9b0644b3a2ab0a0932044fe
mahinulabid1 <himahinulabid@gmail.com>
2024-02-19 20:13:27 +0600

    feat: create component to cache user information

---
+++ src/ui/nav/module/userInfoCache.tsx
@@ -0,0 +1,49 @@
+import {FC, useEffect, useState} from 'react'
+import { useAppSelector, useAppDispatch } from "@store/hooks";
+import { useCookies } from "react-cookie";
+import axios from 'axios'
+import {
+  setBirthdate,
+  setEmail, 
+  setFirstName,
+  setLastName,
+  setGender,
+  setImageData,
+  setUsername,
+  setIsDataCached
+} from '@store/userCacheSlice'
+
+const CacheUser:FC = () => {
+  // const [dbData, setDbData] = useState<any>('No Data!');
+  const [cookies, setCookie, removeCookie] = useCookies<any>(['token']);
+
+  const dispatch = useAppDispatch();
+
+  const isDataCached = useAppSelector((state) => state.user.isDataCached);
+  console.log(isDataCached)
+  // const token = cookies.token;
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG4iLCJpYXQiOjE3MDgzNDQwMjd9.HOQnVO9LWFk-hMTTM0h7BIYQJVRMwlgdiAkpeV-kj9o";
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/v1/user', {headers:{Authorization: `$bearer ${token}`}}).then((res) =>{
+      const data = res.data.data;
+      // console.log(data);
+      // setDbData(res.data.data);
+      dispatch(setBirthdate(data.birthdate));
+      dispatch(setEmail(data.email));
+      dispatch(setFirstName(data.firstName));
+      dispatch(setLastName(data.lastName));
+      dispatch(setGender(data.gender));
+      dispatch(setUsername(data.username));
+      dispatch(setIsDataCached(true));
+      dispatch(setImageData(data.imageCollection))
+    })
+  }, [])
+
+  return(
+    <>
+    </>
+  )
+}
+
+export default CacheUser;
