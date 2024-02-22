3f8ce86dc98ad1c59f378ca43228aa9cd1703b38
mahinulabid1 <himahinulabid@gmail.com>
2024-02-22 20:10:44 +0600

    feat: create invalid route handler

---
+++ src/ui/invalidRoute/invalidRouteScreen.tsx
@@ -0,0 +1,27 @@
+import { FC, useEffect } from "react"
+import styles from "./invalidRouteScreen.module.css"
+import { useNavigate } from "react-router-dom"
+
+
+const InvalidRoute: FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(()=>{
+      navigate('/')
+    }, 6000 ) 
+  }, [])
+
+  return (
+    <>
+      <section className={styles.container}>
+        <h1>404</h1>
+        <p>I'm afraid you've visited a page that doesn't exist on Knigi! 🫤</p>
+        <p>Redirecting to HOMEPAGE</p>
+      </section>
+
+    </>
+  )
+}
+
+export default InvalidRoute;
